988e1e70b96a39858e565754b6b1785eb70f1dc8
xavierloos <xavier.loos04@gmail.com>
2021-02-12 09:46:21 +0000

    If under default returns 40

--- 2nd REVIEW/lib/digital_audio.rb
+++ 2nd REVIEW/lib/digital_audio.rb
@@ -1,3 +1,17 @@
 class DigitalAudio
-  
+  DEFAULT = 40
+  attr_reader :upper_limit, :secArray
+
+  def initialize(upper_limit = 1000)
+    @upper_limit = upper_limit
+    @secArray = []
+  end
+
+  def secuence(i)
+    if i >= DEFAULT  && i <= @upper_limit
+      @secArray << i
+    else i <= DEFAULT
+      @secArray << DEFAULT
+    end
+  end
 end
--- 2nd REVIEW/spec/digital_audio_spec.rb
+++ 2nd REVIEW/spec/digital_audio_spec.rb
@@ -1,4 +1,22 @@
 require "digital_audio"
 RSpec.describe DigitalAudio do
-  let(:da){described_class}
+  let(:da) { described_class.new() }
+  describe "upper_lim" do
+    let(:da) { described_class.new(1500) }
+    it "returns the upper limit" do
+      expect(da.upper_limit).to eq 1500
+    end
+  end
+  describe "secuence" do
+    it "retuns a value of 60 " do
+      expect(da.secArray).to eq []
+      da.secuence(60)
+      expect(da.secArray).to eq [60]
+    end
+    it "returns 40 if under default " do
+      expect(da.secArray).to eq []
+      da.secuence(10)
+      expect(da.secArray).to eq [40]
+    end
+  end
 end
